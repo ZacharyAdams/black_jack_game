77c0564e44c349004a4bec78e5719168d000a7bd
ZacharyAdams <zaekadams@gmail.com>
2014-06-03 13:05:49 -0400

    added shuffle method

--- blackjack.rb
+++ blackjack.rb
@@ -28,7 +28,7 @@ class Deck
   end
 
   def shuffle
-
+    card_deck.shuffle!
   end
 end
 
@@ -45,45 +45,48 @@ class Hand
 
   end
 
-  def score
-    total = 0
-    ace_counter = 0
+#   def score
+#     total = 0
+#     ace_counter = 0
 
-    hand.each do |card|
-    # Strip suit
-    card = card[0..-2]
-    if card = "K", "Q", "J"
-      value = 10
-    elsif card = "A"
-      value = 11
-      ace_counter += 1
-      end
-    end
-    total += value
+#     hand.each do |card|
+#     # Strip suit
+#     card = card[0..-2]
+#     if card = "K", "Q", "J"
+#       value = 10
+#     elsif card = "A"
+#       value = 11
+#       ace_counter += 1
+#       end
+#     end
+#     total += value
 
-    if total > 21 && ace_counter > 0
-      total -= 10
-      ace_counter -= 1
-    end
-    total
-  end
+#     if total > 21 && ace_counter > 0
+#       total -= 10
+#       ace_counter -= 1
+#     end
+#     total
+#   end
 end
 
 
-puts "Welcome to SaZa! Blackjack!\n\n"
-puts "Player was dealt #{Deck.card_deck}\n"
-puts "Player was dealt #{Deck.card_deck}\n"
-puts "Player Score: #{Hand.score}\n\n"
-puts "Hit or stand (h/s)"
-gets.chomp
-puts "Player Score: #{Hand.score}\n\n\n"
-puts "Dealer was dealt #{Deck.card_deck}\n"
-puts "Dealer was dealt #{Deck.card_deck}\n"
-puts "Dealer Score: #{Hand.score}\n\n"
-puts "Hit or stand (h/s)"
-gets.chomp
-puts "Dealer Score: #{Hand.score}\n\n\n"
-puts "#{conditional(win or lose)}"
+#grab a card from the deck and give it to the player and pull it from the array deck modifiying the deck method
+
+
+# puts "Welcome to SaZa! Blackjack!\n\n"
+# puts "Player was dealt #{Deck.card_deck}\n"
+# puts "Player was dealt #{Deck.card_deck}\n"
+# puts "Player Score: #{Hand.score}\n\n"
+# puts "Hit or stand (h/s)"
+# gets.chomp
+# puts "Player Score: #{Hand.score}\n\n\n"
+# puts "Dealer was dealt #{Deck.card_deck}\n"
+# puts "Dealer was dealt #{Deck.card_deck}\n"
+# puts "Dealer Score: #{Hand.score}\n\n"
+# puts "Hit or stand (h/s)"
+# gets.chomp
+# puts "Dealer Score: #{Hand.score}\n\n\n"
+# puts "#{conditional(win or lose)}"
 
 
 
